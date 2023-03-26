4ed6d61ef2e20ba7cded5d4c05fece060de45bd5
PRAVINKAVI <pravinraj71296@gmail.com>
2023-03-26 09:40:42 +0530

    second insert

---
+++ src/variationdate/Datevariation_function.java
@@ -0,0 +1,30 @@
+package variationdate;
+
+public class Datevariation_function {
+	
+	public static int vno;
+	public static int date;
+	public static String plantA;
+	public static String plantB;
+	
+	public  int sales(int v1,int d1)
+	{
+		
+		this.vno=v1;
+		this.date=d1;
+		
+		if()
+		
+		return sale;
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
